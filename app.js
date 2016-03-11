7df1f38c9426f8c5bf0149b35e0e74817761896b
jojino <romanticsion@gmail.com>
2016-03-11 12:14:26 +0900

    <html파일생성>

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+/*
 app.get('/', function(req, res) {
   res.send('Hello World!');
-});
+});*/
+
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'publuc')));
+console.log(__dirname);
 
 app.listen(3000, function() {
   console.log('Server On!');
